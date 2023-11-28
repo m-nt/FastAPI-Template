c09ca690bec72d8deb9e0c33cb1137e00b200957
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-11-28 11:54:23 +0330

    Update test_{{ cookiecutter.project_slug }}_service.py

--- {{cookiecutter.project_name}}/tests/test_{{ cookiecutter.project_slug }}_service.py
+++ {{cookiecutter.project_name}}/tests/test_{{ cookiecutter.project_slug }}_service.py
@@ -1,13 +1,23 @@
 from {{ cookiecutter.project_slug }} import __version__
 import pytest
+from httpx import AsyncClient
 
-from {{ cookiecutter.project_slug }}.handler import Handler
-{{ cookiecutter.resource_name}}_hander = Handler()
+BASE_URL = "http://localhost:8000"
 
 def test_version():
     assert __version__ == "0.1.0"
 
+async def get_test(url, headers, params, status):
+    async with AsyncClient(base_url=BASE_URL) as client:
+        res = await client.get(url=url, headers=headers, params=params)
+    assert res.status_code == status
+
+
+async def post_test(url, headers, params, body, status):
+    async with AsyncClient(base_url=BASE_URL) as client:
+        res = await client.post(url=url, headers=headers, params=params, json=body)
+    assert res.status_code == status
+
 @pytest.mark.asyncio()
 async def test_api_{{ cookiecutter.resource_name }}():
-    result = await {{ cookiecutter.resource_name }}_handler.get_{{ cookiecutter.resource_name }}(data="{{ cookiecutter.project_slug }}")
-    assert result == "{{ cookiecutter.project_slug }}"
+    await get_test("/{{ cookiecutter.resource_name }}", {}, {}, 200)
