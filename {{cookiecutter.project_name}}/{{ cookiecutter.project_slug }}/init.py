f29d6887ef3def848b3c6eba98f56a9cad221284
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-11-28 11:46:40 +0330

    Update init.py

--- {{cookiecutter.project_name}}/{{ cookiecutter.project_slug }}/init.py
+++ {{cookiecutter.project_name}}/{{ cookiecutter.project_slug }}/init.py
@@ -1,20 +1,38 @@
 from dataclasses import dataclass
 
+from starlette.middleware.base import BaseHTTPMiddleware
+
 from fastapi import FastAPI
 
 from {{ cookiecutter.project_slug }}.settings import get_settings
 from {{ cookiecutter.project_slug }}.handler.{{ cookiecutter.resource_name }} import {{ cookiecutter.resource_name }}_Handler
+from {{ cookiecutter.project_slug }}.db import {{cookiecutter.project_slug}}s_collection
 _SETTINGS = get_settings()
 
 
 @dataclass
 class Context:
-    pass
+    {{ cookiecutter.resource_name }}_handler: {{ cookiecutter.resource_name }}_Handler
 
+_{{ cookiecutter.resource_name }}_handler = {{ cookiecutter.resource_name }}_Handler(collection={{cookiecutter.project_slug}}s_collection)
+
+class ExampleMiddleware(BaseHTTPMiddleware):
+    def __init__(
+        self,
+        app,
+    ):
+        super().__init__(app)
+
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        return response
 
 def get_context() -> Context:
-    return Context()
+    return Context(
+        {{ cookiecutter.resource_name }}_handler = _{{ cookiecutter.resource_name }}_handler,
+    )
 
 
 async def set_context(app: FastAPI) -> None:
+    app.add_middleware(ExampleMiddleware)
     app.state.context = get_context()
