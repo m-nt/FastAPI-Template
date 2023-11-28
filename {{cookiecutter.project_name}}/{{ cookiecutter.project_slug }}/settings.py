bc3ae0b06e44b96ec7c93bd699c7500394795b2b
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-11-28 11:47:29 +0330

    Update settings.py

--- {{cookiecutter.project_name}}/{{ cookiecutter.project_slug }}/settings.py
+++ {{cookiecutter.project_name}}/{{ cookiecutter.project_slug }}/settings.py
@@ -8,8 +8,7 @@ class Settings(BaseSettings):
 
     DATABASE_URL: str = "mongodb://localhost:27017"
     SERVICE_NAME: str = "{{ cookiecutter.project_slug }}"
-    CERBOS_URL: str = "http://localhost:3592"
-    CERBOS_POLICY_VERSION: str = "default"
+    ENV_MODE: str = "dev"
 
 
 def get_settings() -> Settings:
